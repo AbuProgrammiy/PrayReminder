eb8aaff7a0fb4ca14e03ab9f61a6ec4494996ec6
Abu_Programmiy <abdukholiq0907@gmail.com>
2024-10-19 07:45:01 +0500

    commit

--- PrayReminder.Application/Services/BackgroundServices/MainBackgroundService.cs
+++ PrayReminder.Application/Services/BackgroundServices/MainBackgroundService.cs
@@ -304,7 +304,7 @@ namespace PrayReminder.Application.Services.BackgroundServices
 
         public async Task SendBotInfo(Message msg)
         {
-            int? usersCount=await _userService.GetAllUsersCount();
+            int? usersCount = await _userService.GetAllUsersCount();
             await _bot.SendTextMessageAsync(msg.Chat.Id, $"Bot egasi @Abu_Programmiy 😁\n\nFoydalanuvchilar soni: {usersCount}\n\nFoydalanilgan ma'nbalar:\nislomapi.uz\naladhan.com");
         }
 
