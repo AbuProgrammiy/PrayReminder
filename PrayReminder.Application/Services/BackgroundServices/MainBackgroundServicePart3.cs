2fd7cf12fb41907ed3165176bbed36650e279a3c
Abu_Programmiy <abdukholiq23@gmail.com>
2024-11-23 18:39:24 +0500

    making more accurate 4

--- PrayReminder.Application/Services/BackgroundServices/MainBackgroundServicePart3.cs
+++ PrayReminder.Application/Services/BackgroundServices/MainBackgroundServicePart3.cs
@@ -1,6 +1,53 @@
-﻿namespace PrayReminder.Application.Services.BackgroundServices
+﻿using Newtonsoft.Json.Linq;
+
+namespace PrayReminder.Application.Services.BackgroundServices
 {
     public partial class MainBackgroundService
     {
+        public async Task CheckPrayTime()
+        {
+            string baseURL = "https://islomapi.uz/api/present/day?region=";
+            HttpClient client = new HttpClient();
+            DateTime currentDateTime = DateTime.Now;
+            TimeOnly currentTime = TimeOnly.FromDateTime(currentDateTime);
+
+            for (byte i = 0; i < regions.Count(); i++)
+            {
+                string data = await client.GetStringAsync(baseURL + regions[i]);
+
+                JObject prayTimes = JObject.Parse(data);
+                string bomdod = prayTimes["times"]["tong_saharlik"].ToString();
+                string quyosh = prayTimes["times"]["quyosh"].ToString();
+                string peshin = prayTimes["times"]["peshin"].ToString();
+                string asr = prayTimes["times"]["asr"].ToString();
+                string shom = prayTimes["times"]["shom_iftor"].ToString();
+                string hufton = prayTimes["times"]["hufton"].ToString();
+
+                if (bomdod == currentTime.ToString("HH:mm"))
+                {
+                    await RemindPrayTime("Bomdod", DefineRegion(regions[i]), bomdod);
+                }
+                else if (quyosh == currentTime.ToString("HH:mm"))
+                {
+                    await RemindPrayTime("Quyosh", DefineRegion(regions[i]), quyosh);
+                }
+                else if (peshin == currentTime.ToString("HH:mm"))
+                {
+                    await RemindPrayTime("Peshin", DefineRegion(regions[i]), peshin);
+                }
+                else if (asr == currentTime.ToString("HH:mm"))
+                {
+                    await RemindPrayTime("Asr", DefineRegion(regions[i]), asr);
+                }
+                else if (shom == currentTime.ToString("HH:mm"))
+                {
+                    await RemindPrayTime("Shom", DefineRegion(regions[i]), shom);
+                }
+                else if (hufton == currentTime.ToString("HH:mm"))
+                {
+                    await RemindPrayTime("Hufton", DefineRegion(regions[i]), hufton);
+                }
+            }
+        }
     }
 }
---
+++ PrayReminder.Application/Services/BackgroundServices/MainBackgroundServicePart4.cs
@@ -0,0 +1,42 @@
+﻿using PrayReminder.Domain.Entities.Enums;
+
+namespace PrayReminder.Application.Services.BackgroundServices
+{
+    public partial class MainBackgroundService
+    {
+        public Region DefineRegion(string regionName)
+        {
+            switch (regionName)
+            {
+                case "Toshkent":
+                    return Region.Toshkent;
+                case "Andijon":
+                    return Region.Andijon;
+                case "Buxoro":
+                    return Region.Buxoro;
+                case "Sirdaryo":
+                    return Region.Sirdaryo;
+                case "Samarqand":
+                    return Region.Samarqand;
+                case "Surxandaryo":
+                    return Region.Surxandaryo;
+                case "Namangan":
+                    return Region.Namangan;
+                case "Navoiy":
+                    return Region.Navoiy;
+                case "Jizzax":
+                    return Region.Jizzax;
+                case "Qashqadaryo":
+                    return Region.Qashqadaryo;
+                case "Farg'ona":
+                    return Region.Fargona;
+                case "Xiva":
+                    return Region.Xiva;
+                case "Qoraqalpog'iston":
+                    return Region.Qoraqalpogiston;
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
